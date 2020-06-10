1923840759740a929d1a4f7afb3ae826a56633a0
Onkanya-20 <onkanya.w@20scoops.net>
2020-06-11 00:19:49 +0700

    use yup for validate

--- src/pages/Example/pages/FinalForm/index.js
+++ src/pages/Example/pages/FinalForm/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
+import { setIn } from 'final-form';
 
 import { required } from 'utils/form/validators';
 
 import { Wrapper, Headline } from './index.view';
 import { AdaptField, AdaptSelect, AdaptTextarea } from 'components/Field';
 import Button from 'components/Button';
-
+import { string, object } from 'yup';
 // import * as yup from 'yup';
 
 // import Dropzone from './dropzone';
@@ -44,25 +45,73 @@ const ExampleFinalForm = () => {
   const onSubmit = value => {
     console.log('value ::', value);
   };
+
+  const validate = async values => {
+    try {
+      await handleRequire().validate(values, { abortEarly: false });
+    } catch (err) {
+      const errors = err.inner.reduce((formError, innerError) => {
+        return setIn(formError, innerError.path, innerError.message);
+      }, {});
+      return errors;
+    }
+  };
+  const handleRequire = () => {
+    // const data = string().required();
+    // return data.isValid(value).then(res => (res ? undefined : 'please'));
+    const schema = object().shape({
+      firstName: string()
+        .required('Please enter your firstname.')
+        .min(5)
+        .max(10),
+      lastName: string()
+        .required('Please enter your lastName.')
+        .email()
+        .min(6)
+    });
+    return schema;
+    // .validate({ firstName: value.firstName, lastName: value.lastName })
+    // .then(res => undefined)
+    // .catch(error => error.errors);
+  };
+
   return (
     <Wrapper>
       <Headline>Simple Form Example</Headline>
       <Form
         onSubmit={onSubmit}
+        validate={values => validate(values)}
         render={({ handleSubmit, form, submitting, pristine, values }) => (
           <form onSubmit={handleSubmit}>
             <Field
               name="firstName"
               label="FirstName"
-              component={AdaptField}
-              validate={required('First Name')}
-            />
+              // subscription={{ error: true, touched: true }}
+              // component={AdaptField}
+              // validate={required('First Name')}
+            >
+              {props => {
+                const { input, meta, ...rest } = props;
+                return (
+                  <>
+                    <AdaptField
+                      {...props}
+                      type="text"
+                      placeholder="FirstName"
+                    />
+                    {/* {(meta.error || meta.submitError) && meta.touched && (
+                      <span>{meta.error || meta.submitError}</span>
+                    )} */}
+                  </>
+                );
+              }}
+            </Field>
 
             <Field
               name="lastName"
               label="LastName"
               component={AdaptField}
-              validate={required('Last Name')}
+              // validate={required('Last Name')}
             />
 
             <Field
